0c19dfd5aba59b1831f3187e7e7c7dc29aa6a6c6
brianaqp_ <brianqp9889@gmail.com>
2025-08-17 11:58:30 -0600

    feat: swarm 3 nodes examples
    Very basic script who launches the most recent ubuntu distro with docker installed

--- examples/aws/swarm-3-nodes/main.tf
+++ examples/aws/swarm-3-nodes/main.tf
@@ -1,5 +1,29 @@
 terraform {
   required_providers {
-    aws = {}
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.9.0"
+    }
+  }
+}
+
+data "aws_ami" "ubuntu_latest" {
+  most_recent = true
+  owners = [ "099720109477" ]
+
+  filter {
+    name = "name"
+    values = [ "ubuntu/images/hvm-ssd-gp3/ubuntu-noble-*-amd64-server-*" ]
+  }
+}
+
+
+resource "aws_instance" "tf-swarm-node" {
+  ami = data.aws_ami.ubuntu_latest.id
+  instance_type = var.instance_type
+  user_data_base64 = base64encode(file("${path.module}/user_data.sh"))
+
+  tags = {
+    Name = "tf-testing"
   }
 }
